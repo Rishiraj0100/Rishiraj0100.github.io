529752338c92442f89ec7a6e42ad018a05b1375a
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2021-09-13 08:55:59 +0530

    Create utils.js

---
+++ utils.js
@@ -0,0 +1,22 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  typeWrite()
+}
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+var i = 0;
+var speed = 50;
+var toWrite = document.getElementById("typewrite");
+function typeWrite() {
+  for (i2 = 0; i2 < toWrite.length; i2++) {
+    i = 0
+    txt = toWrite[i2].innerHTML;
+    toWrite[i2].innerHTML = ''
+    if (i < txt.length) {
+      toWrite[i2].innerHTML += txt.charAt(i);
+      i++;
+      setTimeout(typeWrite, speed);
+    }
+  }
+}
