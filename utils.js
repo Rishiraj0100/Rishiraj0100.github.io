fb907221ec0f8720b8d6967eb74503ea4734e60e
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2021-09-13 09:29:22 +0530

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,21 +1,18 @@
-var i = 0;
 var speed = 50;
 var toWrite = document.getElementsByClassName("typewrite");
-function typeWrite() {
-  for (i2 = 0; i2 < toWrite.length; i2++) {
-    i = 0
-    txt = toWrite[i2].innerHTML;
-    toWrite[i2].innerHTML = ''
-    if (i < txt.length) {
-      toWrite[i2].innerHTML += txt.charAt(i);
-      i++;
-      setTimeout(typeWrite, speed);
-    }
+function typeWrite(elem, txt, i) {
+  if (i < txt.length) {
+    elem.innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWrite, speed);
   }
 }
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
-  typeWrite()
+  for (i2 = 0; i2 < toWrite.length; i2++) {
+    t = toWrite[i2].innerHTML
+    toWrite[i2].innerHTML = ''
+    typeWrite(toWrite[i2], t, 0)
 }
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
