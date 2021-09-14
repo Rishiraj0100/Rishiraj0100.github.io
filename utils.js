756998094719e6e7507db93991b2511201f406a8
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2021-09-14 12:41:09 +0530

    Update utils.js

--- utils.js
+++ utils.js
@@ -8,7 +8,6 @@ const typed = new Typed(".typing", {
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
 }
-
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
