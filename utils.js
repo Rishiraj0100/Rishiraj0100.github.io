50ab495b0498dbec09f250aa7fb4eae7b2772971
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2021-09-14 13:09:30 +0530

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,10 +1,15 @@
 const typed = new Typed(".typing", {
-  strings: ["Coder", "Discord Bot Creator", "Web Developer", "Student"],
+  strings: ["Coder", "Pythonist", "Discord Bot Creator", "Web Developer", "Student", "Human"],
   loop: true,
   typeSpeed: 80,
   backSpeed: 40,
 });
-
+const typed2 = new Typed(".codetyping", {
+  strings: ["Python", "HTML", "CSS", "JavaScript"],
+  loop: true,
+  typeSpeed: 80,
+  backSpeed: 40,
+});
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
 }
