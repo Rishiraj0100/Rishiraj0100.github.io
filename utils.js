cb6df9d8ce99f8a432dcd383ddf2e324f14cc7ce
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2021-09-14 15:08:18 +0530

    Update utils.js

--- utils.js
+++ utils.js
@@ -17,12 +17,7 @@ const typed = new Typed(".typing", {
   typeSpeed: 80,
   backSpeed: 40,
 });
-const typed2 = new Typed(".codetyping", {
-  strings: ["Python", "HTML", "CSS", "JavaScript"],
-  loop: true,
-  typeSpeed: 80,
-  backSpeed: 40,
-});
+
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
 }
