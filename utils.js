cf3117c1918d749ac18a084e4c0cd6f8fd240811
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2021-09-14 12:30:40 +0530

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,6 +1,21 @@
+const typed = new Typed(".typing", {
+  strings: [
+    "Coder",
+    "Pythonist",
+    "Discord Bot Creator",
+    "Web Developer",
+    "Student",
+    "Human"
+  ],
+  loop: true,
+  typeSpeed: 80,
+  backSpeed: 40,
+});
+
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
 }
+
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
