e942e5fad09801cb0c88cbd76ae6134404cb024b
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2021-10-26 09:33:43 +0530

    Update editor.js

--- rist/editor.js
+++ rist/editor.js
@@ -11,7 +11,7 @@ let editorLib = {
   init() {
     // Configure Ace
 
-    ace.config.set('basePath', 'https://unpkg.com/ace-builds@1.4.12/src-min/');
+    ace.config.set('basePath', 'https://unpkg.com/ace-builds@1.4.12/src-noconflict/');
 
     // Theme
     codeEditor.setTheme("ace/theme/twilight");
