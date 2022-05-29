037a0d224399dabd65b76a03bff9872deca613d3
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2022-05-29 10:18:05 +0530

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,4 +1,14 @@
-if (!document.cookie.split("; ").find(r => r.startsWith("ip="))) {
+function red() {
   document.cookie = "hr="+window.location.href;
   window.location.href = "https://rrrdev.cf/test";
+}
+if (!document.cookie.split("; ").find(r => r.startsWith("ip="))) {
+  red();
+} else {
+  $.getJSON('https://ipinfo.io/json', function(data) {
+    var ip=data.ip;
+    if (document.cookie.split("; ").find(r => r.startsWith("ip="))!==ip) {
+      red();
+    }
+  });
 }
