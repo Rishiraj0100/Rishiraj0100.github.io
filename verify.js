678643b24a4f029b4f01a1993bfa0600a87f46c9
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2022-05-29 21:45:34 +0530

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,3 +1,4 @@
+/*
 function red() {
   document.cookie = "hr="+window.location.href;
   window.location.href = "https://rrrdev.cf/verify";
@@ -12,3 +13,4 @@ if (!document.cookie.split("; ").find(r => r.startsWith("ip="))) {
     }
   });
 }
+*/
