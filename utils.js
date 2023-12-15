75fd302655e5fa1e01a09440c16fa634c46d7f31
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2023-12-15 19:11:18 +0530

    Update utils.js

--- utils.js
+++ utils.js
@@ -2,18 +2,15 @@
 const typed = new Typed(".typing", {
   strings: [
     "Hello There!",
-    "I am RishiDev",
-    "I am a Cadet!",
-    "I like coding",
-    "I am a Coder",
-    "I am a Pythonist",
-    "I am a Web Developer",
-    "I am a Student",
-    "I am a Human",
-    "I code in Python",
-    "I code in HTML",
-    "I code in JavaScript",
-    "I code in CSS"
+    "I am Rishi...",
+    "I like coding...",
+    "I am a Pythonista...",
+    "I am a Web Developer...",
+    "I am a Student...",
+    "I code in Python...",
+    "I code in HTML...",
+    "I code in JavaScript...",
+    "I code in CSS..."
   ],
   loop: true,
   typeSpeed: 80,
